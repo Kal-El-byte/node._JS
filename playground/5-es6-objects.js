a1d4000fc775cf3a0e6433f751af7c2a10e3d400
Uchiha Itachi <danivetlyrics@gmail.com>
2023-05-26 21:59:45 +0100

    fourth

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -4,8 +4,21 @@ const name = "Andrew";
 const userAge = 27;
 
 const user = {
-    name: name,
-    age: userAge
+    name,
+    age: userAge,
+    location: 'Philadelphia'
 }
 
 console.log(user);
+
+//Object destructuring
+
+const product = {
+    label: 'Red Notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
+const {label, price, stock, salePrice, rating = '3' } = product;
+console.log(product);
